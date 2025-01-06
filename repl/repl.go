2c81ab9ef40be95458461c900e5d0d4b8403bb88
ruegerj <jan.rueger@gmx.ch>
2025-01-06 20:21:23 +0100

    add basic evaluator for integer literals

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ruegerj/monkey/evaluator"
 	"github.com/ruegerj/monkey/lexer"
 	"github.com/ruegerj/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
